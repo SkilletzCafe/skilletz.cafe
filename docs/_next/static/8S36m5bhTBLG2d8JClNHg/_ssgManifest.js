02c5ab7f806e46d813e5b00732917d6796ecae07
Jonathan Tsai <hello@jontsai.com>
2025-09-08 01:32:21 -0700

    Deploy 9c49f51156c2a63aa5ae19244c9cfb7f4ddfabbe

--- docs/_next/static/8S36m5bhTBLG2d8JClNHg/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fblog\u002F[slug]","\u002Fblog\u002Ftag\u002F[tagId]","\u002Fmenu","\u002Fmenu\u002Fbrunch-print","\u002Fmenu\u002Fdinner-print","\u002Fmenu\u002Fgrabngo-print","\u002Ftv","\u002Ftvrekz"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ docs/_next/static/t5_ukLE_MfJaz-XFBxV48/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fblog\u002F[slug]","\u002Fblog\u002Ftag\u002F[tagId]","\u002Fmenu","\u002Fmenu\u002Fbrunch-print","\u002Fmenu\u002Fdinner-print","\u002Fmenu\u002Fgrabngo-print","\u002Fmenu\u002Ftearekz-print","\u002Ftv","\u002Ftvrekz"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
