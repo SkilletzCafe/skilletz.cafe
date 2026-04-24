682dea53fdf55c2da054358b7c52706a24c90074
Jonathan Tsai <jontsai@users.noreply.github.com>
2026-04-24 14:41:44 -0700

    fix: load Google Analytics site-wide (#26)
    * fix: load Google Analytics site-wide
    * refactor: remove unused local Google Analytics component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,7 @@
+import { GoogleAnalytics } from '@hacktoolkit/nextjs-htk/components';
+
+import { ANALYTICS } from '@/config/analytics';
+
 import StructuredData from '@/components/StructuredData';
 
 import '@/styles/globals.css';
@@ -5,6 +9,7 @@ import '@/styles/globals.css';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <GoogleAnalytics measurementId={ANALYTICS.google.measurementId} />
       <StructuredData />
       <Component {...pageProps} />
     </>
